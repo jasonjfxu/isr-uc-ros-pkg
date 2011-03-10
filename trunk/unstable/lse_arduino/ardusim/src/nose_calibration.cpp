229fd750bd5286964a2a65096779aed687e73932
goncabrita@gmail.com <goncabrita@gmail.com@c69aaeaa-51a7-1f1b-0fba-500eb5f5969d>
2011-03-10 18:28:13 +0000

    Updatedthe Calibration Nose software.

--- trunk/unstable/lse_arduino/ardusim/src/nose_calibration.cpp
+++ trunk/unstable/lse_arduino/ardusim/src/nose_calibration.cpp
@@ -41,51 +41,253 @@
 #include <vector>
 #include <string>
 #include <list>
-
-#include <iostream> 
+#include <iostream>
 #include <sys/ioctl.h> 
 #include <fcntl.h> 
-#include <linux/kd.h> 
+#include <linux/kd.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
 
 #include "ros/ros.h"
 #include "lse_sensor_msgs/Nostril.h"
 #include "cereal_port/CerealPort.h"
+#include "std_msgs/UInt16.h"
 
-// Global variables
-cereal::CerealPort serial_port;
-
-struct Nose
+class NoseCalibration
 {
-	lse_sensor_msgs::Nostril nostril;
-	FILE * csv_file;
-	std::list<int> readings;
-};
+	public:
+	NoseCalibration();
+	~NoseCalibration();
 	
-std::vector<Nose> noses;
-double temperature;
-double humidity;
+	void userInterface();
+	void mainLoop();
+	
+	private:
+	
+	// Serial Port
+	cereal::CerealPort serial_port;
+	
+	ros::NodeHandle n;
+	ros::NodeHandle pn;
+	
+	ros::Subscriber sub;
+	
+	struct Nose
+	{
+		lse_sensor_msgs::Nostril nostril;
+		FILE * csv_file;
+		std::list<int> readings;
+		ros::Publisher pub;
+	};
+	std::vector<Nose> noses;
+	
+	double temperature;
+	double humidity;
+	
+	double chemical_volume;
+	
+	bool fill_buffer;
+	
+	// Parameters
+	std::string port;
+	int baudrate;
+	
+	double volume;
+	double density;
+	double molar_volume;
+	double molar_mass;
+	
+	int num_noses;
+	int buffer_size;
+	
+	double fan_on_time;
+	double fan_off_time;
+	
+	// Functions
+	void noseCallback(const lse_sensor_msgs::Nostril::ConstPtr& msg);
+	void arduinoCallback(std::string * data);
+	
+	void fanOn();
+	void fanOff();
+	void beep();
+	void closeFiles();
+};
 
-bool fill_buffer;
-int buffer_size;
+NoseCalibration::NoseCalibration() : n(), pn("~"), serial_port()
+{
+	pn.param<std::string>("port", port, "/dev/ttyUSB0");
+	pn.param("baudrate", baudrate, 9600);
+	
+	pn.param("box_volume", volume, 1.0);
+	pn.param("gas_density", density, 1.0);
+	pn.param("gas_molar_volume", molar_volume, 1.0);
+	pn.param("gas_molar_mass", molar_mass, 1.0);
+	
+	pn.param("number_of_noses", num_noses, 1);
+	pn.param("number_of_samples", buffer_size, 10);
+	
+	pn.param("fan_on_time", fan_on_time, 10.0);
+	pn.param("fan_off_time", fan_off_time, 3.0);
+	
+	// Serial port
+	try{ serial_port.open((char*)port.c_str(), baudrate); }
+	catch(cereal::Exception& e)
+	{
+		ROS_FATAL("eNose Calibration -- Failed to open serial port!");
+		ROS_BREAK();
+	}
+	ROS_INFO("eNose Calibration -- Successfully connected to the arduino!");
+	
+	if( !serial_port.startReadBetweenStream(boost::bind(&NoseCalibration::arduinoCallback, this, _1), '@', 'e') )
+	{
+		ROS_FATAL("eNose Calibration -- Failed to start streaming data!");
+		ROS_BREAK();
+	}
+	ROS_INFO("eNose Calibration -- Starting to stream data...");
+	
+	fill_buffer = false;
+	sub = n.subscribe("nose", 100, &NoseCalibration::noseCallback, this);
+	
+	chemical_volume = 0.0;
+	
+	// Wait for all noses...
+	ROS_INFO("eNose Calibration -- Waiting for all noses to come online...");
+	ros::Rate r(1.0);
+	while(n.ok() && noses.size()<num_noses)
+	{
+		ros::spinOnce();
+		r.sleep();
+	}
+	
+	ROS_INFO("eNose Calibration -- Got all noses!");
+}
 
-// Helper functions
-void noseCallback(const lse_sensor_msgs::Nostril::ConstPtr& msg)
+NoseCalibration::~NoseCalibration()
+{
+	serial_port.close();
+}
+
+void NoseCalibration::userInterface()
+{ 
+	ROS_INFO("eNose Calibration - %s - User Interface", __FUNCTION__);
+
+	ros::Rate r(1.0);
+	char input[8];
+		
+	while(n.ok())
+	{
+		if(!fill_buffer)
+		{
+			printf("[QUERY] How much chemical did you add? ");
+			scanf("%s", input);
+			getchar();
+		
+			if(input[0]=='q' || input[0]=='Q')
+			{
+				closeFiles();
+				ROS_INFO("eNose Calibration - %s - Press CTRL+C to exit.", __FUNCTION__);
+				return;
+			}
+		
+			chemical_volume += (double)atoi(input);
+		
+			fanOn();
+		
+			if(input[0]=='a' || input[0]=='A')
+			{
+				ros::Duration(fan_on_time).sleep();
+			}
+			else
+			{
+				printf("[QUERY] Press ENTER to stop the fan... ");
+				fflush(stdout);
+				getchar();
+			}
+		
+			fanOff();
+			ros::Duration(fan_off_time).sleep();
+			beep();
+		
+			fill_buffer = true;
+		}
+		r.sleep();
+	}
+}
+
+void NoseCalibration::mainLoop()
+{
+	ROS_INFO("eNose Calibration - %s - Main Loop", __FUNCTION__);
+
+	ros::Rate r(1.0);
+	std::vector<Nose>::iterator n_it;
+		
+	while(n.ok())
+	{
+		if(fill_buffer)
+		{
+			bool found_uncomplete_buffer = false;
+			for(n_it=noses.begin() ; n_it!=noses.end() ; n_it++)
+			{
+				if(n_it->readings.size() < buffer_size)
+				{
+					found_uncomplete_buffer = true;
+					break;
+				}
+			}
+		
+			if(!found_uncomplete_buffer)
+			{
+				fill_buffer = false;
+				double ppm;
+			
+				for(n_it=noses.begin() ; n_it!=noses.end() ; n_it++)
+				{
+					n_it->readings.sort();
+					while(n_it->readings.size() > buffer_size/2) n_it->readings.pop_front();
+			
+					ppm = (molar_volume/molar_mass)*(density*chemical_volume*1000/volume);
+								
+					fprintf(n_it->csv_file, "%.6lf,%d,%.2lf,%.2lf\n", ppm, n_it->readings.front(), temperature, humidity);
+				
+					n_it->readings.clear();
+				}
+				
+				ROS_INFO("eNose Calibration - %s - Wrote to file. Now at %duL %.3lfppm", __FUNCTION__, (int)chemical_volume, ppm);
+			}
+		}
+		
+		for(n_it=noses.begin() ; n_it!=noses.end() ; n_it++)
+		{
+			std_msgs::UInt16 view_nose_msg;
+			view_nose_msg.data = n_it->nostril.raw_data;
+			n_it->pub.publish(view_nose_msg);
+		}
+		
+		ros::spinOnce();
+		r.sleep();
+	}
+}
+
+void NoseCalibration::noseCallback(const lse_sensor_msgs::Nostril::ConstPtr& msg)
 {
 	bool nostril_found = false;
-	std::vector<Nose>::iterator n;
-	for(n=noses.begin() ; n!=noses.end() ; n++)
+	std::vector<Nose>::iterator n_it;
+	for(n_it=noses.begin() ; n_it!=noses.end() ; n_it++)
 	{
-		if(n->nostril.header.frame_id.compare(msg->header.frame_id)==0 && n->nostril.sensor_model.compare(msg->sensor_model)==0)
+		if(n_it->nostril.header.frame_id.compare(msg->header.frame_id)==0 && n_it->nostril.sensor_model.compare(msg->sensor_model)==0)
 		{
-			n->nostril.header.stamp = msg->header.stamp;
-			n->nostril.reading = msg->reading;
+			n_it->nostril.header.stamp = msg->header.stamp;
+			n_it->nostril.raw_data = msg->raw_data;
 			
 			if(fill_buffer)
 			{
-				n->readings.push_front((int)msg->reading);
-				if(n->readings.size() > buffer_size) n->readings.pop_back();
+				n_it->readings.push_front((int)msg->raw_data);
+				if(n_it->readings.size() > buffer_size) n_it->readings.pop_back();
 			}
-		
+			
 			nostril_found = true;
 			break;
 		}
@@ -95,12 +297,13 @@ void noseCallback(const lse_sensor_msgs::Nostril::ConstPtr& msg)
 	{
 		ROS_INFO("eNose Calibration - %s - Found a new nostril %s %s", __FUNCTION__, msg->header.frame_id.c_str(), msg->sensor_model.c_str());
 	
+		Nose new_nose;
+	
 		std::string file_name;
 		
 		file_name.append(msg->sensor_model);
-		file_name.append("_");
+		//file_name.append("_");
 		file_name.append(msg->header.frame_id);
-		file_name.append(".csv");
 		
 		// Lets take all '/' and ' ' out!
 		for(int i=0 ; i<file_name.size() ; i++)
@@ -108,9 +311,13 @@ void noseCallback(const lse_sensor_msgs::Nostril::ConstPtr& msg)
 			if(file_name[i] == '/' || file_name[i] == ' ') file_name[i] = '_';
 		}
 		
+		ROS_INFO("eNose Calibration - %s - Publishing raw data on topic /%s...", __FUNCTION__, file_name.c_str());
+		
+		new_nose.pub = n.advertise<std_msgs::UInt16>(file_name, 10);
+		
 		ROS_INFO("eNose Calibration - %s - Opening file %s...", __FUNCTION__, file_name.c_str());
 		
-		Nose new_nose;
+		file_name.append(".csv");
 		new_nose.csv_file = fopen(file_name.c_str(), "w");
 		new_nose.nostril = *msg;
 		
@@ -118,26 +325,32 @@ void noseCallback(const lse_sensor_msgs::Nostril::ConstPtr& msg)
 	}
 }
 
-void newDataCallback(std::string * data)
+void NoseCalibration::arduinoCallback(std::string * data)
 {
 	sscanf(data->c_str(), "@,%lf,%lf,e", &temperature, &humidity);
 	
 	//ROS_INFO("eNose Calibration - %s - %s T:%lf H:%lf", __FUNCTION__, data->c_str(), temperature, humidity);
 }
 
-void fanOn()
+void NoseCalibration::fanOn()
 {
 	ROS_INFO("eNose Calibration - %s - Turning the fan on...", __FUNCTION__);
 	serial_port.write("@1");
 }
 
-void fanOff()
+void NoseCalibration::fanOff()
 {
 	serial_port.write("@0");
 	ROS_INFO("eNose Calibration - %s - Turned the fan off.", __FUNCTION__);
 }
 
-void closeFiles()
+void NoseCalibration::beep()
+{
+	serial_port.write("@b");
+	printf("%c", 7);
+}
+
+void NoseCalibration::closeFiles()
 {
 	if(noses.size()>0)
 	{
@@ -151,139 +364,21 @@ void closeFiles()
 	}
 }
 
-void beep()
-{
-	printf("%c", 7);
-}
-
 // Main
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "nose_calibration");
-	ros::NodeHandle n;
-	ros::NodeHandle pn("~");
 	
 	ROS_INFO("eNose Calibration Node for ROS v0.1");
 	
-	// Parameters
-	std::string port;
-	int baudrate;
+	NoseCalibration nc;
 	
-	double volume;
-	double density;
-	double molar_volume;
-	double molar_mass;
+	boost::thread t(boost::bind(&NoseCalibration::userInterface, &nc));
 	
-	int num_noses;
-	double fan_on_time;
-	double fan_off_time;
+	nc.mainLoop();
 	
-	pn.param<std::string>("port", port, "/dev/ttyUSB0");
-	pn.param("baudrate", baudrate, 9600);
+	t.join();
 	
-	pn.param("box_volume", volume, 1.0);
-	pn.param("gas_density", density, 1.0);
-	pn.param("gas_molar_volume", molar_volume, 1.0);
-	pn.param("gas_molar_mass", molar_mass, 1.0);
-	
-	pn.param("number_of_noses", num_noses, 1);
-	pn.param("number_of_samples", buffer_size, 10);
-	pn.param("fan_on_time", fan_on_time, 10.0);
-	pn.param("fan_off_time", fan_off_time, 3.0);
-
-	// Serial port
-	try{ serial_port.open((char*)port.c_str(), baudrate); }
-	catch(cereal::Exception& e)
-	{
-		ROS_FATAL("eNose Calibration -- Failed to open serial port!");
-		ROS_BREAK();
-	}
-	ROS_INFO("eNose Calibration -- Successfully connected to the arduino!");
-	
-	if( !serial_port.startReadBetweenStream(boost::bind(&newDataCallback, _1), '@', 'e') )
-	{
-		ROS_FATAL("eNose Calibration -- Failed to start streaming data!");
-		ROS_BREAK();
-	}
-	ROS_INFO("eNose Calibration -- Starting to stream data...");
-	
-	ros::Subscriber sub = n.subscribe("nose", 100, noseCallback);
-	
-	ros::Rate r(1.0);
-	
-	// Wait for all noses...
-	ROS_INFO("eNose Calibration -- Waiting for all noses to come online...");
-	while(n.ok() && noses.size()<num_noses)
-	{
-		ros::spinOnce();
-		r.sleep();
-	}
-	
-	std::vector<Nose>::iterator n_it;
-	
-	// Main loop
-	double chemical_volume = 0.0;
-	fill_buffer = false;	
-	while(n.ok())
-	{
-		if(!fill_buffer)
-		{
-			char input[8];
-			printf("[QUERY] How much chemical did you add? ");
-			scanf("%s", input);
-			
-			if(input[0]=='q' || input[0]=='Q')
-			{
-				closeFiles();
-				ROS_INFO("eNose Calibration -- Goodbye!");
-				return 0;
-			}
-			
-			chemical_volume += (double)atoi(input);
-			
-			fanOn();
-			ros::Duration(fan_on_time).sleep();
-			fanOff();
-			ros::Duration(fan_off_time).sleep();
-			beep();
-			
-			fill_buffer = true;
-		}
-		else
-		{
-			bool found_uncomplete_buffer = false;
-			for(n_it=noses.begin() ; n_it!=noses.end() ; n_it++)
-			{
-				if(n_it->readings.size() < buffer_size)
-				{
-					found_uncomplete_buffer = true;
-					break;
-				}
-			}
-			
-			if(!found_uncomplete_buffer)
-			{
-				for(n_it=noses.begin() ; n_it!=noses.end() ; n_it++)
-				{
-					n_it->readings.sort();
-					while(n_it->readings.size() > buffer_size/2) n_it->readings.pop_front();
-				
-					double ppm = (molar_volume/molar_mass)*(density*chemical_volume*1000/volume);
-				
-					fprintf(n_it->csv_file, "%.6lf,%d,%.2lf,%.2lf\n", ppm, n_it->readings.front(), temperature, humidity);
-					
-					n_it->readings.clear();
-				}
-				
-				fill_buffer = false;
-			}
-		}
-	
-		ros::spinOnce();
-		r.sleep();
-	}
-	
-	closeFiles();
 	ROS_INFO("eNose Calibration -- Goodbye!");
 	return 0;
 }
