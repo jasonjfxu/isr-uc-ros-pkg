dd04bffd83fd7bba7e4d87d8c5f12efe361287a5
goncabrita <goncabrita@gmail.com>
2013-06-24 13:24:44 +0000

    Updating the squirtle_robot with improved multiplexed teleoperation and velocity publishing for the localization module.

--- stacks/squirtle_robot/trunk/squirtle_driver/src/squirtle_localization_node.cpp
+++ stacks/squirtle_robot/trunk/squirtle_driver/src/squirtle_localization_node.cpp
@@ -42,14 +42,17 @@
 #include <tf/transform_broadcaster.h>
 #include <message_filters/subscriber.h>
 
-//#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Odometry.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/NavSatFix.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
 
 #include <rtk_ros/UTMConversion.h>
 
 double true_north_compensation;
+
+nav_msgs::Odometry last_odom;
 
 tf::TransformListener * tf_listener;
 
@@ -140,7 +143,7 @@ int main(int argc, char** argv)
 	tf_listener = new tf::TransformListener();
 
 	message_filters::Subscriber<sensor_msgs::Imu> * imu_sub = new message_filters::Subscriber<sensor_msgs::Imu>();
-	imu_sub->subscribe(n, "imu/data", 20);
+	imu_sub->subscribe(n, "imu", 20);
     tf::MessageFilter<sensor_msgs::Imu> * tf_filter_imu = new tf::MessageFilter<sensor_msgs::Imu>(*imu_sub, *tf_listener, odom_frame_id, 20);
     tf_filter_imu->registerCallback( boost::bind(imuCallback, _1) );
 
@@ -153,6 +156,8 @@ int main(int argc, char** argv)
 	odom_sub->subscribe(n, "odom", 20);
     tf::MessageFilter<nav_msgs::Odometry> * tf_filter = new tf::MessageFilter<nav_msgs::Odometry>(*odom_sub, *tf_listener, base_footprint_frame_id, 20);
     tf_filter->registerCallback( boost::bind(odomCallback, _1) );*/
+
+    ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("global_odom", 10);
 
 	static tf::TransformBroadcaster tf_broadcaster;
 
@@ -167,6 +172,46 @@ int main(int argc, char** argv)
         t_world_odom.setRotation(t_world_odom_rotation.getRotation());
 
         tf_broadcaster.sendTransform(tf::StampedTransform(t_world_odom, ros::Time::now(), global_frame_id, odom_frame_id));
+
+        // Odometry header
+        nav_msgs::Odometry odom_msg;
+        odom_msg.header.frame_id = global_frame_id;
+        odom_msg.header.stamp = ros::Time::now();
+
+        // Odometry position
+        tf::Vector3 position = t_world_odom_translation.getOrigin();
+        odom_msg.pose.pose.position.x = position.getX();
+        odom_msg.pose.pose.position.y = position.getY();
+        odom_msg.pose.pose.position.z = position.getZ();
+
+        // Odometry orientation
+        tf::quaternionTFToMsg(t_world_odom_rotation.getRotation(), odom_msg.pose.pose.orientation);
+
+        // TODO: Finish this, add covariance...
+        //odom_msg.pose.covariance...
+
+        ros::Duration delta_t = odom_msg.header.stamp - last_odom.header.stamp;
+
+        if(delta_t.toSec() < 1.0)
+        {
+            // Odometry linear velocity
+            odom_msg.twist.twist.linear.x = (odom_msg.pose.pose.position.x - last_odom.pose.pose.position.x)/delta_t.toSec();
+            odom_msg.twist.twist.linear.y = (odom_msg.pose.pose.position.y - last_odom.pose.pose.position.y)/delta_t.toSec();
+            odom_msg.twist.twist.linear.z = (odom_msg.pose.pose.position.z - last_odom.pose.pose.position.z)/delta_t.toSec();
+
+            // TODO: Fix this... We are assuming that the robot cannot rotate fast enought, so shortest_angular_distance might work.
+            // Odometry angular velocity
+            odom_msg.twist.twist.angular.x = 0.0;
+            odom_msg.twist.twist.angular.y = 0.0;
+            odom_msg.twist.twist.angular.z = (angles::shortest_angular_distance(tf::getYaw(odom_msg.pose.pose.orientation), tf::getYaw(last_odom.pose.pose.orientation)))/delta_t.toSec();
+
+            // TODO: Finish this, add covariance...
+            //odom_msg.twist.covariance...
+
+            odom_pub.publish(odom_msg);
+        }
+
+        last_odom = odom_msg;
 
 		ros::spinOnce();
 		r.sleep();
